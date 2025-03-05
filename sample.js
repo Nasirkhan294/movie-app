8e7431b91e561405e37038e86a93c1eda971b855
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2025-03-05 17:40:59 +0500

    Fetch and render the movie data using javascript

--- sample.js
+++ sample.js
@@ -2,11 +2,9 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
-
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
-
 
 // Get initial movies
 getMovies(API_URL)
@@ -21,24 +19,23 @@ async function getMovies(url) {
 function showMovies(movies) {
 	main.innerHTML = ''
 
-	movies.forEach(movie => {
-		const {title, poster_path, vote_average, overview} = movie
+	movies.forEach((movie) => {
+		const { title, poster_path, vote_average, overview } = movie
 
 		const movieEl = document.createElement('div')
 		movieEl.classList.add('movie')
 
 		movieEl.innerHTML = `
-		    <img src="${IMG_PATH + poster_path}" alt="${title}">
-			<div class="movie-info">
-			  <h3 class='movie-info'>${title}</h3>
-			  <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-			</div>
-			<div class="overview">
-			  <h3>Overview</h3>
-			  ${overview}
-			</div>
-		`
-
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
 		main.appendChild(movieEl)
 	})
 }
